2c0b71ee74c5d0a93597093036a47da797d378a1
guojiacheng <417719323@qq.com>
2016-11-14 19:58:33 +0800

    studio 第一次提交

---
+++ app/src/main/java/com/exam/erweima/MainActivity.java
@@ -0,0 +1,18 @@
+package com.exam.erweima;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.zxing.encoding.EncodingHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
+
