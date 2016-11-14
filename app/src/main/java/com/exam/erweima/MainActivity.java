4fa922f05ce978a84f252148b71d0ad07c438444
guojiacheng <417719323@qq.com>
2016-11-14 20:28:43 +0800

    克隆后第一次修改

--- app/src/main/java/com/exam/erweima/MainActivity.java
+++ app/src/main/java/com/exam/erweima/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //克隆后的修改，首次提交
     }
 }
 
